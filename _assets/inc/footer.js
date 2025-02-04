002eaa61555a8a00f473861d1dec99d1311da8e2
本間智久 <honmat1974@gmail.com>
2025-02-05 08:32:36 +0900

    休業のお知らせ

--- _assets/inc/footer.js
+++ _assets/inc/footer.js
@@ -5,11 +5,11 @@ var _html =
 			'<img src="/_assets/images/common/footer_logo.png" alt="UMI THE K 古宇利島">' +
 		'</a>' +
 		'<p class="address">〒905-0406 沖縄県古宇利 2427-1</p>' +
-		'<p class="tel">Tel／Fax： 0980-56-3997 <br class="disp_sp">（電話でお問い合わせ 10:00〜18:00）</p>' +
-		'<p class="notice">※臨時休業・営業時間変更となる場合があります。<br>※時間外のお問い合わせにつきましては、下記CONTACT（お問い合わせ）よりお願いいたします。<br>※お電話でのご予約は承っておりませんので、下記RESERVATION（宿泊予約）よりお待ちしております。</p>' +
+		// '<p class="tel">Tel／Fax： 0980-56-3997 <br class="disp_sp">（電話でお問い合わせ 10:00〜18:00）</p>' +
+		// '<p class="notice">※臨時休業・営業時間変更となる場合があります。<br>※時間外のお問い合わせにつきましては、下記CONTACT（お問い合わせ）よりお願いいたします。<br>※お電話でのご予約は承っておりませんので、下記RESERVATION（宿泊予約）よりお待ちしております。</p>' +
 		'<div class="btn_wrap">' +
-			'<a href="https://www7.489pro.com/asp/489/menu.asp?id=47000056&lan=JPN" target="_blank" class="btn footer"><span class="eng">RESERVATION</span>宿泊予約</a>' +
-			'<a href="/contact/" class="btn footer"><span class="eng">CONTACT</span>お問い合せ</a>' +
+			//'<a href="https://www7.489pro.com/asp/489/menu.asp?id=47000056&lan=JPN" target="_blank" class="btn footer"><span class="eng">RESERVATION</span>宿泊予約</a>' +
+			// '<a href="/contact/" class="btn footer"><span class="eng">CONTACT</span>お問い合せ</a>' +
 		'</div>' +
 		'<div class="instagram_wrap">' +
 			'<a href="https://www.instagram.com/umi_the_k_kourijima/" target="_blank"><img src="/_assets/images/common/footer_instagram.png" alt="instagram @umi_the_k_kourijima"></a>' +
